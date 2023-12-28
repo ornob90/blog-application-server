64baabfc89844ed1b7afae15e8833858a3f2b0c5
ornob90 <oornob49@gmail.com>
2023-12-28 20:40:48 +0600

    get all blog posts api

---
+++ src/api/blog/getAllBlogs.js
@@ -0,0 +1,17 @@
+const { default: BlogPost } = require("../../models/blog");
+
+const getAllBlogs = async (req, res, next) => {
+  try {
+    const blogPosts = await BlogPost.find();
+    res.send({
+      status: "success",
+      message: "Data retrieved",
+      blogPosts,
+    });
+  } catch (error) {
+    console.error(error.message);
+    next(error);
+  }
+};
+
+module.exports = getAllBlogs;
--- src/routes/blog/index.js
+++ src/routes/blog/index.js
@@ -1,6 +1,10 @@
 const addBlog = require("../../api/blog/addBlog");
+const getAllBlogs = require("../../api/blog/getAllBlogs");
 
 const router = require("express").Router();
+
+// GET
+router.get("/api/blogs", getAllBlogs);
 
 // POST
 router.post("/api/blog-post", addBlog);
